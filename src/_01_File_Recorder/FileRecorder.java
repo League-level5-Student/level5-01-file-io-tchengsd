1f0405281f57552399cd84823f12da19bfc947ca
tchen <tchen@LAPTOP-999U7F6K.lan1>
2020-10-03 14:22:45 -0700

    File Recorder (IP)

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,21 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Enter a message to add to the file");
+		char[] chars = input.toCharArray();
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/output.txt");
+			fw.write(chars);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
